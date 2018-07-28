0df45437ebe1057e17e6466c708ee3bfed705099
May Ann Dagundon <mayanndagundon199x@gmail.com>
2018-07-28 17:46:18 +0800

    Adding all files

---
+++ app/src/main/java/com/dagundon/a03_laboratory_exercise_1/meh.java
@@ -0,0 +1,13 @@
+package com.dagundon.a03_laboratory_exercise_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class meh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meh);
+    }
+}
